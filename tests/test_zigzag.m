13c974485b233c42ed43c13a34a91bf740bf1004
Julien Fischer <juliensf@gmail.com>
2025-12-31 03:48:59 +1100

    More work on testing.
    tests/test_zigzag.m:
        As above.

--- tests/test_zigzag.m
+++ tests/test_zigzag.m
@@ -28,6 +28,7 @@
 :- import_module int.
 :- import_module int16.
 :- import_module int32.
+:- import_module int64.
 :- import_module int8.
 :- import_module list.
 :- import_module string.
@@ -41,18 +42,10 @@ main(!IO) :-
     test_all_int32s(!IO),
 
     % Exhaustive testing is not feasible for 64-bit integers.
-    do_test64(-9223372036854775808i64, !IO),
-    do_test64(-1i64, !IO),
-    do_test64(0i64, !IO),
-    do_test64(1i64, !IO),
-    do_test64(9223372036854775807i64, !IO),
-    io.nl(!IO),
-
-    do_test(-2147483648, !IO),
-    do_test(-1, !IO),
-    do_test(0, !IO),
-    do_test(1, !IO),
-    do_test(2147483647, !IO).
+    % We test a list of known values.
+    % XXX and then should test some at random.
+    test_int64s(!IO),
+    test_ints(!IO).
 
 %---------------------------------------------------------------------------%
 
@@ -73,7 +66,8 @@ test_all_int8s_loop(N, !NumFailures, !IO) :-
     EncodedN = encode_int8(N),
     DecodedN = decode_int8(EncodedN),
     ( if N \= DecodedN then
-        io.format("FAILED: encode_int8(%di8) = %uu8; decode_int8(%uu8) = %di8\n",
+        io.format(
+            "FAILED: encode_int8(%di8) = %uu8; decode_int8(%uu8) = %di8\n",
             [i8(N), u8(EncodedN), u8(EncodedN), i8(DecodedN)], !IO),
         !:NumFailures = !.NumFailures + 1
     else
@@ -104,7 +98,8 @@ test_all_int16s_loop(N, !NumFailures, !IO) :-
     EncodedN = encode_int16(N),
     DecodedN = decode_int16(EncodedN),
     ( if N \= DecodedN then
-        io.format("FAILED: encode_int16(%di16) = %uu16; decode_int16(%uu16) = %di16\n",
+        io.format(
+            "FAILED: encode_int16(%di16) = %uu16; decode_int16(%uu16) = %di16\n",
             [i16(N), u16(EncodedN), u16(EncodedN), i16(DecodedN)], !IO),
         !:NumFailures = !.NumFailures + 1
     else
@@ -135,7 +130,8 @@ test_all_int32s_loop(N, !NumFailures, !IO) :-
     EncodedN = encode_int32(N),
     DecodedN = decode_int32(EncodedN),
     ( if N \= DecodedN then
-        io.format("FAILED: encode_int32(%di32) = %uu32; decode_int32(%uu32) = %di32\n",
+        io.format(
+            "FAILED: encode_int32(%di32) = %uu32; decode_int32(%uu32) = %di32\n",
             [i32(N), u32(EncodedN), u32(EncodedN), i32(DecodedN)], !IO),
         !:NumFailures = !.NumFailures + 1
     else
@@ -149,21 +145,90 @@ test_all_int32s_loop(N, !NumFailures, !IO) :-
 
 %---------------------------------------------------------------------------%
 
-:- pred do_test64(int64::in, io::di, io::uo) is det.
+:- pred test_int64s(io::di, io::uo) is det.
 
-do_test64(N, !IO) :-
+test_int64s(!IO) :-
+    some [!NumFailures] (
+        !:NumFailures = 0,
+        test_non_random_int64s(!NumFailures, !IO),
+        ( if !.NumFailures = 0 then
+            io.print_line("PASSED: non-random int64 tests.", !IO)
+        else
+            true
+        )
+    ).
+
+:- pred test_non_random_int64s(int::in, int::out, io::di, io::uo) is det.
+
+test_non_random_int64s(!NumFailures, !IO) :-
+    list.foldl2(do_test_int64, int64s, !NumFailures, !IO).
+
+:- pred do_test_int64(int64::in, int::in, int::out, io::di, io::uo) is det.
+
+do_test_int64(N, !NumFailures, !IO) :-
     EncodedN = encode_int64(N),
     DecodedN = decode_int64(EncodedN),
-    io.format("encode_int64(%d) = %u; decode_int64(%u) = %d\n",
-        [i64(N), u64(EncodedN), u64(EncodedN), i64(DecodedN)], !IO).
+    ( if N \= DecodedN then
+        io.format(
+        "FAILED: encode_int64(%di64) = %uu64; decode_int64(%uu64) = %di64\n",
+        [i64(N), u64(EncodedN), u64(EncodedN), i64(DecodedN)], !IO),
+        !:NumFailures = !.NumFailures + 1
+    else
+        true
+    ).
 
-:- pred do_test(int::in, io::di, io::uo) is det.
+:- func int64s = list(int64).
 
-do_test(N, !IO) :-
+int64s = [
+    min_int64,
+    -1i64,
+    0i64,
+    1i64,
+    max_int64
+].
+
+%---------------------------------------------------------------------------%
+
+:- pred test_ints(io::di, io::uo) is det.
+
+test_ints(!IO) :-
+    some [!NumFailures] (
+        !:NumFailures = 0,
+        test_non_random_ints(!NumFailures, !IO),
+        ( if !.NumFailures = 0 then
+            io.print_line("PASSED: non-random int tests.", !IO)
+        else
+            true
+        )
+    ).
+
+:- pred test_non_random_ints(int::in, int::out, io::di, io::uo) is det.
+
+test_non_random_ints(!NumFailures, !IO) :-
+    list.foldl2(do_test_int, ints, !NumFailures, !IO).
+
+:- pred do_test_int(int::in, int::in, int::out, io::di, io::uo) is det.
+
+do_test_int(N, !NumFailures, !IO) :-
     EncodedN = encode_int(N),
     DecodedN = decode_int(EncodedN),
-    io.format("encode_int(%d) = %u; decode_int(%u) = %d\n",
-        [i(N), u(EncodedN), u(EncodedN), i(DecodedN)], !IO).
+    ( if N \= DecodedN then
+        io.format("FAILED: encode_int(%d) = %uu; decode_int(%uu) = %d\n",
+            [i(N), u(EncodedN), u(EncodedN), i(DecodedN)], !IO),
+        !:NumFailures = !.NumFailures + 1
+    else
+        true
+    ).
+
+:- func ints = list(int).
+
+ints = [
+    min_int,
+    -1,
+    0,
+    1,
+    max_int
+].
 
 %---------------------------------------------------------------------------%
 :- end_module test_zigzag.
